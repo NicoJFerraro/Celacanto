61e8c6353b3ccf35d9b603f72e1178c7576f29cb
Nico Ferraro <nicolasjesus.ferraro@gmail.com>
2023-09-16 04:30:10 -0300

    Project Set Up

---
+++ Source/Celacanto/CelacantoGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CelacantoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CELACANTO_API ACelacantoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
